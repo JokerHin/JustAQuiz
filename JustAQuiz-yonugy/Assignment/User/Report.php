306a07dd0cfe5d8e8ecb6d8d026b019c3ee4d341
Cho Kar Hin <158465774+JokerHin@users.noreply.github.com>
2024-12-16 14:39:58 +0800

    Add files via upload

---
+++ JustAQuiz-yonugy/Assignment/User/Report.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JustAQuiz</title>
+    <link rel="stylesheet" href="Report.css">
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <div id="h1">JUST</div><div id="h2">A</div><div id="h3">QUIZ</div>
+        </div>
+    </header>
+
+    <nav class="navbar">
+        <a href="Home.php">HOME</a>
+        <a href="Option.php">QUIZZES</a>
+        <a href="DashBoard.php">DASHBOARD</a>
+        <a href="MyProfile.php">MY PROFILE</a>
+        <a href="Login.php">LOGOUT</a>
+    </nav>
+    <main>
+        <div id="main"> 
+            <div class="header">123456 Yong Wai</div>
+            <div class="container">
+                <div class="content">
+                    <div class="report">
+                        <h2>Overall Report</h2>
+                        <div class="info">
+                            <div class="info-item">
+                                <div class="label">Average Score:</div>
+                                <div class="value">85%</div>
+                            </div>
+                            <div class="info-item">
+                                <div class="label">Total Quizzes Completed:</div>
+                                <div class="value">10</div>
+                            </div>
+                            <div class="info-item">
+                                <div class="label">Average Grade:</div>
+                                <div class="value">A</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="right">
+                        <h2>Badges</h2>
+                        <div class="badges">
+                            <div class="box">
+                                <img src="../images/CSS-Badge.png" alt="CSS Knight Badge">
+                                <p>CSS Knight</p>
+                            </div>
+                            <div class="box">
+                                <img src="../images/HTML-Badge.png" alt="CSS Knight Badge">
+                                <p>CSS Knight</p>
+                            </div>
+                            <div class="box">
+                                <img src="../images/HTML-Badge.png" alt="CSS Knight Badge">
+                                <p>CSS Knight</p>
+                            </div>
+                            <div class="box">
+                                <img src="../images/HTML-Badge.png" alt="CSS Knight Badge">
+                                <p>CSS Knight</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <ul class="bg-bubbles">
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+</body>
+</html>
